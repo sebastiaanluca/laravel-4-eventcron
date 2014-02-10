f62382295da808115f8fd77459ed0f79a3a63be6
Sebastiaan Luca <git@sebastiaanluca.com>
2014-02-10 23:33:17 +0100

    Lots of refactoring
    Carbon integration
    Some name changes
    Database refactoring
    Execution time per queue item and in total
    New status: being processed

--- src/Cossou/EventCRON/models/EventCRON.php
+++ src/Cossou/EventCRON/models/EventCRON.php
@@ -1,6 +1,7 @@
 <?php namespace Cossou\EventCRON\Models;
 
 use App;
+use Carbon\Carbon;
 use Config;
 use Event;
 use Exception;
@@ -12,14 +13,14 @@ class EventCron extends \Event {
 	/**
 	 * Register a callback for a given event.
 	 *
-	 *
-	 * @param  string $queue
+	 * @param  string $event
 	 * @param  array $data
 	 * @param  int $date
 	 * @return int
 	 */
-	public static function queueDB($queue, $data = array(), $date = 0) {
-		return EventCronBase::create(array('queue' => $queue, 'arguments' => serialize($data), 'date' => $date));
+	public static function queue($event, $data = array(), Carbon $executeAt = NULL) {
+		if(is_null($executeAt)) $executeAt = Carbon::now();
+		return EventCronBase::create(array('event' => $event, 'arguments' => serialize($data), 'execute_at' => $executeAt));
 	}
 
 	/**
@@ -87,29 +88,35 @@ class EventCron extends \Event {
 			/**
 			 * @var $queues Collection
 			 */
-			$queues = EventCronBase::where('processed', '=', FALSE)
-				->where('date', '<=', date('Y-m-d H:i:s'))
+			$queues = EventCronBase::where('status', '=', EventCRONBase::UNPROCESSED_STATUS)
+				->where('execute_at', '<=', Carbon::now())
 				->orderBy('created_at', 'ASC')
 				->take(Config::get('eventcron::config.max_events_per_execution'))
 				->get();
 
-			foreach($queues as $queue) {
-				$queue->started_at = (microtime(TRUE) * 10000);
-				$queue->runned_at = date('Y-m-d H:i:s');
+			$totalExecutionTime = 0;
 
+			foreach($queues as $queue) {
+				$queue->started_at = Carbon::now();
+				$queue->status = EventCRONBase::PROCESSING_STATUS;
+				$queue->save(); // Extra query though
+
+				// Fire away!
 				Event::fire($queue->queue, unserialize($queue->arguments));
 
+				$queue->status = EventCRONBase::PROCESSED_STATUS;
+				$queue->ended_at = Carbon::now();
+				$queue->save(); // Save again
+
 				if(Config::get('eventcron::config.log_events')) {
-					Log::info('Event [' . $queue->queue . '-' . $queue->id . ']  was executed!');
+					Log::info('Event [' . $queue->event . '-' . $queue->id . '] was executed! Execution time (in seconds): ' . $queue->getExecutionTime());
 				}
 
-				$queue->processed = TRUE;
-				$queue->ended_at = (microtime(TRUE) * 10000);
-				$queue->save();
+				$totalExecutionTime += $queue->getExecutionTime();
 			}
 
 			if(Config::get('eventcron::config.log_events')) {
-				Log::info('Cronjob ended!');
+				Log::info('Cronjob ended! Total execution time (in seconds): ' . $totalExecutionTime);
 			}
 
 			return $queues->count();
--- src/Cossou/EventCRON/models/EventCRONBase.php
+++ src/Cossou/EventCRON/models/EventCRONBase.php
@@ -3,4 +3,14 @@
 class EventCRONBase extends \Eloquent {
 	protected $table = 'eventcron';
 	protected $guarded = array('id');
+	protected $dates = array('execute_at', 'started_at', 'ended_at');
+
+	/* */
+	const UNPROCESSED_STATUS = 0;
+	const PROCESSING_STATUS = 4;
+	const PROCESSED_STATUS = 8;
+
+	public function getExecutionTime() {
+		return $this->ended_at->diffInSeconds($this->started_at);
+	}
 }
